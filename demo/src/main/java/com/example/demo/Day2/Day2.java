d2c22026d44b5ebcfbe4a88ac8e2a226d6b1e64d
PrabhuArun <142887874+PrabhuArun@users.noreply.github.com>
2023-08-25 12:38:20 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Day2/Day2.java
@@ -0,0 +1,14 @@
+package com.example.demo.Day2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Day2 {
+   @Value("${val}")
+   public String username;
+   @GetMapping("/disp")
+   public String display() {
+	   return username;}
+}
